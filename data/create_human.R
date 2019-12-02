6093274f723ee43dfa3859e7b106b8610bec734e
vinnu0143 <tonduru.kumar@gmail.com>
2019-12-02 23:26:30 +0200

    final

--- data/create_human.R
+++ data/create_human.R
@@ -23,8 +23,8 @@ colnames(hd)
 
 #Rename the columns with my own new short cuts
 
-new_hd <- c("HDI_Rank","Country","HDI","Life_Exp","Edu_Exp","Edu_Mean","GNI","GNI_Minus_Rank")
-new_gii <- c("GII_Rank", "Country", "GII","Mat_Mor","Ad_Birth","Parli_F","Edu2F","Edu2M","LabF","LabM")
+new_hd <- c("HDI.Rank","Country","HDI","Life.Exp","Edu.Exp","Edu.Mean","GNI","GNI.Minus.Rank")
+new_gii <- c("GII.Rank", "Country", "GII","Mat.Mor","Ado.Birth","Parli.F","Edu2.F","Edu2.M","Labo.F","Labo.M")
 
 colnames(hd) <-new_hd 
 colnames(gii) <-new_gii  
@@ -34,8 +34,8 @@ colnames(gii)
 
 library(dplyr); library(ggplot2)
 
-gii <- mutate(gii, Edu2FM = (Edu2F / Edu2M))
-gii <- mutate(gii, LabFM = (LabF / LabM))
+gii <- mutate(gii, Edu2.FM = (Edu2.F / Edu2.M))
+gii <- mutate(gii, Labo.FM = (Labo.F / Labo.M))
 
 colnames(gii)
 
@@ -49,3 +49,31 @@ human<- read.csv("C:/Users/vinnu/Documents/IODS-project/data/human.csv")
 colnames(human)
 
 #The final dataset contains 195 observations and 19 variables.
+
+
+#Chapter5 
+
+colnames(human)
+
+human <- mutate(human, as.numeric(GNI))
+keep <- c("Country", "Edu2.FM", "Labo.FM", "Life.Exp", "Edu.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F")
+human <- select(human, one_of(keep))
+
+complete.cases(human)
+human_ <- filter(human, complete.cases(human)== TRUE)
+
+
+human$Country
+
+last <- nrow(human_) - 7
+human_ <- human_[1:last, ]
+
+rownames(human_) <- human_$Country
+human_ <- human_[ ,2:ncol(human_)]
+
+dim(human_)
+human <- human_
+
+write.csv(human, file="C:/Users/vinnu/Documents/IODS-project/data/human.csv", row.names= TRUE)
+human<- read.csv("C:/Users/vinnu/Documents/IODS-project/data/human.csv")
+colnames(human)
